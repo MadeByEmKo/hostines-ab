97d92f795c56186820c3dd470ec1c45e6688d157
Hostinec AB <hostinecab@web.dev>
2026-05-23 21:51:58 +0200

    fix: change base path typo from hostines-ab to hostinec-ab to match renamed repository

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://MadeByEmKo.github.io',
-  base: process.env.GITHUB_ACTIONS ? '/hostines-ab' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/hostinec-ab' : '/',
   output: 'static',
 });
